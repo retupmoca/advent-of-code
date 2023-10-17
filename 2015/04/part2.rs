e0d09ab46ff6556befdad72680930475aaab9c28
Andrew <andrew@egeler.us>
2023-10-17 19:29:08 -0400

    2015.04

---
+++ 2015/04/part2.rs
@@ -0,0 +1,25 @@
+#!/usr/bin/env rust-script
+//! ```cargo
+//! [dependencies]
+//! md5 = "0.7"
+//! ```
+
+use std::io::stdin;
+
+fn main() {
+    let input = stdin().lines().next().unwrap().unwrap();
+    let input = input.trim_end();
+
+    let mut num = 1;
+    while !check_hash(input, num) {
+        num += 1;
+    }
+
+    println!("num: {}", num);
+}
+
+fn check_hash(prefix: &str, num: usize) -> bool {
+    let to_hash = format!("{}{}", prefix, num);
+    let hashed = format!("{:x}", md5::compute(to_hash));
+    return hashed.starts_with("000000");
+}
